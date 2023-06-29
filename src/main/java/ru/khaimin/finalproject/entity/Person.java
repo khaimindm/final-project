5ba081e48334e809a37d7efb92936ca4febdcccc
khaimindm <khaimindm@gmail.com>
2023-06-29 12:03:37 +0000

    29.06.2023

---
+++ src/main/java/ru/khaimin/finalproject/entity/Person.java
@@ -0,0 +1,73 @@
+package ru.khaimin.finalproject.entity;
+
+import java.time.LocalDate;
+
+public class Person {
+    private int id;
+    private String login;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
+    private String role;
+
+    public Person() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }    
+    
+}
