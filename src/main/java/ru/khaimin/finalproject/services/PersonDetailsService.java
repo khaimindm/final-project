70bf04b9ceb3af624e1a8262eb5bd0db48eafa8a
Dmitrii Khaimin <khaimindm@gmail.com>
2023-07-02 18:40:34 +0400

    02.07.2023

--- src/main/java/ru/khaimin/finalproject/services/PersonDetailsService.java
+++ src/main/java/ru/khaimin/finalproject/services/PersonDetailsService.java
@@ -1,7 +1,5 @@
 package ru.khaimin.finalproject.services;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,6 +9,8 @@ import org.springframework.stereotype.Service;
 import ru.khaimin.finalproject.entity.Person;
 import ru.khaimin.finalproject.repositories.PeopleRepository;
 import ru.khaimin.finalproject.security.PersonDetails;
+
+import java.util.Optional;
 
 @Service
 public class PersonDetailsService implements UserDetailsService {
@@ -23,11 +23,15 @@ public class PersonDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<Person> person = peopleRepository.findByLogin(username);       
+        Optional<Person> person = peopleRepository.findByLogin(username);
 
-        if (person.isEmpty()) {
+        if (person == null) {
             throw new UsernameNotFoundException("Пользователь не найден!");
         }
+
+        /*if (person.isEmpty()) {
+            throw new UsernameNotFoundException("Пользователь не найден!");
+        }*/
 
         return new PersonDetails(person.get());
     }
