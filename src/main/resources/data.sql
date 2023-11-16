57cfa73f43f090f8c4bea3d681569d03774e2139
Dmitrii Khaimin <khaimindm@gmail.com>
2023-11-16 22:13:16 +0400

    16.11.23_2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,8 +17,10 @@ INSERT INTO professional_activity (specialty_name, degree_certificate, date_of_e
 INSERT INTO professional_activity (specialty_name, degree_certificate, date_of_employment, persons_person_id) VALUES ('Хирург', '123', '1990-05-05', '6');
 
 INSERT INTO work_time (date_of_work, work_time_start_at, work_time_end_at, availability, specialty_name, persons_person_id) VALUES('2023-10-10', '08:00', '08:30', false, 'Терапевт', '3');
-INSERT INTO work_time (date_of_work, work_time_start_at, work_time_end_at, availability, specialty_name, persons_person_id) VALUES('2024-02-12', '08:00', '08:30', true, 'Анестезиолог', '2');
+INSERT INTO work_time (date_of_work, work_time_start_at, work_time_end_at, availability, specialty_name, persons_person_id) VALUES('2023-10-20', '08:00', '08:30', false, 'Кардиолог', '2');
 
 INSERT INTO booking_list (persons_person_id, work_time_id, completed) VALUES('7', '1', true);
+INSERT INTO booking_list (persons_person_id, work_time_id, completed) VALUES('7', '2', true);
 
 INSERT INTO medical_cards (examination, diagnosis, assigned_therapy, date_of_appointment, specialist_id, booking_list_id) VALUES('Насморк, боли в горле, головная боль', 'Острое респираторное заболевание', 'Обильное питье, постельный режим', '2023-10-10', '3', '1');
+INSERT INTO medical_cards (examination, diagnosis, assigned_therapy, date_of_appointment, specialist_id, booking_list_id) VALUES('Боль в грудной клетке при физической нагрузке. Во время ходьбы боль, резкая усталость в мышцах ног. Боль проходит, если остановиться. Но возникает вновь при продолжении ходьбы.', 'Атеросклероз. Атеросклероз коронарных артерий. Атеросклероз артерий нижних конечностей.', 'Аторвастатин. 10 мг 1 раз/сут.', '2023-10-20', '2', '2');
